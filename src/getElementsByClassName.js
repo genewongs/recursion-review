32762a2e3bfefa62976a129f90fb192b25b54fc0
Gene Wong <gene.h.wong@gmail.com>
2022-02-22 15:27:20 -0800

    Changed spec for getElementsByClassNameSpec and started to implement algorithm for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,35 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
   // your code here
+  // declare a result array, this is our return value
+  var node = node || document.body;
+  var result = [];
+  // crate edge case, if input is invalid, return empty array.
+  // if the element has classname, push the current element into the result array and return res
+
+  // if(node.classList.length !== 0) {
+  //   if(node.classList.contains(className)) {
+  //     result.push(node);
+  //   }
+  // }
+  var nodeClass = node.classList;
+  console.log(node.hasChildNodes())
+
+  if(nodeClass.length >= 1 && nodeClass.contains(className)) {
+    result.push(node);
+  }
+
+  if (node.hasChildNodes()) {
+    var children = node.childNodes;
+    for (var i = 0; i < children.length; i++) {
+      result.concat(getElementsByClassName(className, children[i]))
+    }
+  }
+  //if it has children, iterate all the children to find the classname - recursively call the function on each child
+  //traverse the body if there is children
+  console.log(result);
+  return result;
 };
